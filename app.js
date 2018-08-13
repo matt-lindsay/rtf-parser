5a4703831a35fe460a8d9e7f777f19e90fbd7128
Matt Lindsay <matt_epilp@hotmail.com>
2018-08-13 11:53:35 +0100

    Linting.

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
+'use strict';
+
 const fs = require('fs');
 const parsertf = require('rtf-parser');
-const Rem1Service = require('./services/Rem1Service.js');
+// const Rem1Service = require('./src/services/Rem1Service.js');
 
-let filename = './rtf-files/Summons May 2018.rtf';
+let filename = './resources/rtf-files/NDR REM2017 - APRIL18.rtf';
 
 // New instance of rtf-parser module.
 parsertf.stream(fs.createReadStream(filename), (err, doc) => {
   if (err) {
     throw err;
   } else {
-    
+
     // Array to hold each part of the textual data.
-    let rtfline = []; 
+    let rtfline = [];
     // Empty line item to write to prior to writing to the rtfline array.
     let lineitem = '';
-    
+
     // Loop through the parsed rtf document...
     doc.content.forEach(item => {
       item.content.forEach(record => {
@@ -25,7 +27,7 @@ parsertf.stream(fs.createReadStream(filename), (err, doc) => {
       // Then clear the lineitem variable before the next loop iteration.
       lineitem = '';
     });
-    
+
     // Create an empty output array for reformatted data.
     let outputfile = [];
     // Couter for identifying the first record in the data.
@@ -38,16 +40,16 @@ parsertf.stream(fs.createReadStream(filename), (err, doc) => {
       if (item === 'R1X' || item === 'REM2' || item === 'XXFLAT' || item === 'FINALX') {
         // ...add a newline prior to the start of a new record. The first record will require its
         // preceeding newline character to be removed.
-	// Set reminder boolean value.
-	if (item === 'XXFLAT') {
-	  reminder = false;
-	}
-        if (counter === 0 ) {
+        // Set reminder boolean value.
+        if (item === 'XXFLAT') {
+          reminder = false;
+        }
+        if (counter === 0) {
           // If it is the first record, add to the output array.
           outputfile.push(item);
         } else {
           // If it is the start of a new record, preceed it with a line feed.
-          outputfile.push('\n' + item); // '\n' + 
+          outputfile.push('\n' + item); // '\n' +
         }
       } else {
         // ...add each subsequent item to the CSV.
@@ -56,56 +58,56 @@ parsertf.stream(fs.createReadStream(filename), (err, doc) => {
       // Increment the counter by 1.
       counter++;
     });
-    
+
     // Equalising record lengths: ensure that each record has the same number of fields.
     // If field 11 is blank, remove it as this is an additional address line where 6 or more
     // address fields occur.
     let normalisedOutputFile = [];
     let i = 0; // Zero based record position counter to identify the 11th record.
     outputfile.forEach(item => {
-	// Remove any ',' characters from each item.
-        // Then repeat incase there is more than one ',' per string item.
-	item = item.replace(',', ' ');
-	item = item.replace(',', ' ');
-	// Split summons court cost figures into two fields.
-	item = item.replace('Cost (Authority)', ',Cost (Authority)');
+      // Remove any ',' characters from each item.
+      // Then repeat incase there is more than one ',' per string item.
+      item = item.replace(',', ' ');
+      item = item.replace(',', ' ');
+      // Split summons court cost figures into two fields.
+      item = item.replace('Cost (Authority)', ',Cost (Authority)');
 
-        if (item.match(/R1X/) || item.match(/REM2/) || item.match(/FINALX/) || item.match(/XXFLAT/)) {
-          normalisedOutputFile.push(item);
-          i = 0; // Reset the counter when a new record is identified.
-        } else {
-          if (i === 10 && reminder === true) {
-            if (item.length === 0) {
-              // Ignore it.
-              console.log('>>> IGNORED ' + item);
-            } else {
-              normalisedOutputFile.push(item);
-            }
-          } else if (i === 16 && reminder === true) { // Make space for records with a second financial year.
-            if (item === 'amounts') {
-              normalisedOutputFile.push('');
-              normalisedOutputFile.push(item);
-            } else {
-              normalisedOutputFile.push(item);
-            }
-	  } else if (i === 9 && reminder === false) { // Make space for records with an extra address field.
-            if (item.includes('day')) {
-	      normalisedOutputFile.push('');
-              normalisedOutputFile.push(item);
-	    } else {
-	      //normalisedOutputFile.push('');
-              normalisedOutputFile.push(item);
-	    }
+      if (item.match(/R1X/) || item.match(/REM2/) || item.match(/FINALX/) || item.match(/XXFLAT/)) {
+        normalisedOutputFile.push(item);
+        i = 0; // Reset the counter when a new record is identified.
+      } else {
+        if (i === 10 && reminder === true) {
+          if (item.length === 0) {
+            // Ignore it.
+            console.log('>>> IGNORED ' + item);
           } else {
             normalisedOutputFile.push(item);
           }
+        } else if (i === 16 && reminder === true) { // Make space for records with a second financial year.
+          if (item === 'amounts') {
+            normalisedOutputFile.push('');
+            normalisedOutputFile.push(item);
+          } else {
+            normalisedOutputFile.push(item);
+          }
+        } else if (i === 9 && reminder === false) { // Make space for records with an extra address field.
+          if (item.includes('day')) {
+            normalisedOutputFile.push('');
+            normalisedOutputFile.push(item);
+          } else {
+            //normalisedOutputFile.push('');
+            normalisedOutputFile.push(item);
+          }
+        } else {
+          normalisedOutputFile.push(item);
         }
+      }
       //}
       i++;
     });
 
     // Write the compiled CSV data in outputfile to a file.
-    fs.writeFile('./output/processedRtf.csv', normalisedOutputFile, 'utf8', (err) => {
+    fs.writeFile('./resources/output/processedRtf.csv', normalisedOutputFile, 'utf8', (err) => {
       if (err) {
         throw err;
       } else {
